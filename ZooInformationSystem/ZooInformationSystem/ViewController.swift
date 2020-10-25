089632eaf8aafa7ee50b1fd0ad77127723be40eb
Jessie <jessie.griffin@icloud.com>
2020-10-24 21:50:17 -0400

    Setting up objects

--- ZooInformationSystem/ZooInformationSystem/ViewController.swift
+++ ZooInformationSystem/ZooInformationSystem/ViewController.swift
@@ -5,9 +5,16 @@
 //  Created by Jessie Ann Griffin on 10/24/20.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
+
+    let maintenanceScheduleOne = Schedule(date: Date(), time: Date(), events: [], type: "Grassy and Dry")
+    
+    let habitatOne = Habitat(plants: ["Trees", "Tundra", "Boulders"], acreage: 110, hydration: "Pond", maintenance: maintenanceScheduleOne, animals: [])
+
+    let animal = Animal(species: "Lion", size: 250, name: "Gerard", habitat: habitatOne, diet: ["Cebu", "Gazelle"], compatibility: [])
 
     override func viewDidLoad() {
         super.viewDidLoad()
