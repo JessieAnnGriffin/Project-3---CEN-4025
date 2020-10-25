57997b52feebb39d0eaf378e82ca6df81ba7ce52
Jessie <jessie.griffin@icloud.com>
2020-10-24 21:29:36 -0400

    Habitat class is complete

---
+++ ZooInformationSystem/ZooInformationSystem/Habitat.swift
@@ -0,0 +1,51 @@
+//
+//  Habitat.swift
+//  ZooInformationSystem
+//
+//  Created by Jessie Ann Griffin on 10/24/20.
+//
+
+import Foundation
+
+class Habitat {
+    private var plants: [String] // plant life
+    private var acreage: Int // size
+    private var hydration: String // water source
+    private var maintenance: Schedule // maintenance schedule
+    private var animals: [Animal]
+
+    init(plants: [String] = [],
+         acreage: Int,
+         hydration: String,
+         maintenance: Schedule,
+         animals: [Animal]) {
+
+        self.plants = plants
+        self.acreage = acreage
+        self.hydration = hydration
+        self.maintenance = maintenance
+        self.animals = animals
+    }
+
+    func getVegetation() {
+        print("\(plants)")
+    }
+
+    func setSize(acreage: Int) {
+        self.acreage = acreage
+        print("The baitat is \(acreage) acres.")
+    }
+
+    func performMaintenance() {
+        print("Maintenance has been performed according to the \(maintenance.type)")
+    }
+
+    func testWater() {
+        print("The treatment of the \(hydration) is acceptable")
+    }
+
+    func addAnimal(animal: Animal) {
+        animals.append(animal)
+        print("A \(animal) has been added to the habitat")
+    }
+}
