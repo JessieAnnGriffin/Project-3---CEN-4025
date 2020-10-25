125010b96d542551ee62025ba21c37c1491a8470
Jessie <jessie.griffin@icloud.com>
2020-10-24 20:41:50 -0400

    New project created because playgrounds wasn't recognizing classes

---
+++ ZooInformationSystem/ZooInformationSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZooInformationSystem
+//
+//  Created by Jessie Ann Griffin on 10/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
